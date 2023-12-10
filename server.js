faded10e0758d92f548d589aa5e849aa0119a54d
DmcpEI <diogomcpaixao@gmail.com>
2023-12-10 17:53:59 +0000

    Mudanças

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve the index.html file
 app.get('/', async (req, res) => {
-    res.render('index');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// 404 Page
+// Serve the processo.html file
+app.get('/processo', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'processo.html'));
+});
+
+// Serve the 404.html file for any other routes
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen (PORT , () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
